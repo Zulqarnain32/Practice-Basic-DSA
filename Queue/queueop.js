d50e444e016af48edee9856829569ac7be1f829f
Zulqarnain <zulqarnainchohan250@gmail.com>
2024-05-23 23:55:14 -0700

    added queue operation

--- Queue/queueop.js
+++ Queue/queueop.js
@@ -1,27 +1,47 @@
 const queue = [];
 
 let queueLength = queue.length;
-console.log(queueLength);
 
 
 
 function enqueue(elm){
   queue[queueLength] = elm
   queueLength++
-  console.log(queue);
+  // console.log(queue);
 }
 
 function dequeue(){
     for(let i = 0; i < queueLength-1; i++){
         queue[i] = queue[i+1]
     }
-    queue.length-=1
-    console.log(queue);
+    queueLength--
+    queue.length = queueLength
+    // console.log("after del ", queue);
    
 }
 
-enqueue(100)
-enqueue(200)
-enqueue(300)
-dequeue();
-dequeue();
+function front(){
+   console.log(queue[0]);
+}
+
+function rear(){
+  console.log(queue[queueLength -1]);
+}
+
+function display(){
+  console.log(queue);
+}
+
+function isEmpty(){
+  return queue.length === 0
+}
+
+
+// enqueue(100)
+// enqueue(200)
+// enqueue(300)
+// dequeue();
+// dequeue();
+
+
+// front()
