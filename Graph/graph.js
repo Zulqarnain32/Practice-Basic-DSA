bc892f87e4af023297c9ed0f852a825cbf9c03fc
Zulqarnain <zulqarnainchohan250@gmail.com>
2024-05-26 08:54:57 -0700

    added edges in graph

--- Graph/graph.js
+++ Graph/graph.js
@@ -6,6 +6,18 @@ class Graph {
    addVertex(val){
        this.data[val] = []
    }
+
+   addEdge(v1,v2){
+     if(!this.data[v1]){
+      this.addVertex(v1)
+     }
+     if(!this.data[v2]){
+      this.addVertex(v2)
+     }
+     this.data[v1].push(v2)
+     this.data[v2].push(v1)
+
+   }
 }
 
 const graph = new Graph()
@@ -13,4 +25,10 @@ graph.addVertex("A")
 graph.addVertex("B")
 graph.addVertex("C")
 graph.addVertex("D")
+
+graph.addEdge("A","C")
+graph.addEdge("A","B")
+graph.addEdge("B","D")
+graph.addEdge("B","C")
+
 console.log(graph.data);
