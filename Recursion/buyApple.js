e6457d92d1041380f10b324a723084f884a3d40e
Zulqarnain <zulqarnainchohan250@gmail.com>
2024-05-28 22:39:12 -0700

    buy apple

--- Recursion/buyApple.js
+++ Recursion/buyApple.js
@@ -1,1 +1,18 @@
-let money = 100;
+let money = 50;
+let totalApple = 0;
+let applePrice = 5
+
+function buyApple(money){
+   if(money >= applePrice){
+    money-=applePrice;
+    totalApple++
+    console.log("i have ", totalApple, "apple");
+    console.log("and moneyleft is ", money);
+      buyMore(money)
+   }
+}
+buyApple(money)
+
+function buyMore(moneyLeft){
+    buyApple(moneyLeft)
+}
